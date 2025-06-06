000d431432221d257f1d84572737d08dd0893135
saksham <106744619+saksham011023@users.noreply.github.com>
2025-06-06 23:42:52 +0530

    Update extract_faces.py

--- extract_faces.py
+++ extract_faces.py
@@ -1,16 +1,4 @@
-"""
-Extract faces
 
-Video Face Manipulation Detection Through Ensemble of CNNs
-
-Image and Sound Processing Lab - Politecnico di Milano
-
-Nicolò Bonettini
-Edoardo Daniele Cannas
-Sara Mandelli
-Luca Bondi
-Paolo Bestagini
-"""
 import argparse
 import sys
 import traceback
